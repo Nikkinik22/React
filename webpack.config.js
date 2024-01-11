b9d1bb8ff978305073026c6e13178028b34711c4
User <vyugov98@bk.ru>
2024-01-11 19:56:03 +0300

    iijij

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const clientConfig = require('./cfg/webpack.clinet.config')
+const serverConfig = require('./cfg/webpack.server.config')
+
+module.exports = [
+    clientConfig,
+    serverConfig
+]
