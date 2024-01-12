5e10d19cc43283dacc9cbe89aeccf31a1ed64699
User <vyugov98@bk.ru>
2024-01-12 19:43:43 +0300

    iijij

--- cfg/webpack.clinet.config.js
+++ cfg/webpack.clinet.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const {HotModuleReplacementPlugin} = require('webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 
 const NODE_ENV = process.env.NODE_ENV;
@@ -15,11 +17,18 @@ module.exports = {
     mode: NODE_ENV ? NODE_ENV : 'development',
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+         'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
+        }
     },
-    entry: path.resolve(__dirname, '..src/client/index.jsx'),
+    entry: [
+        path.resolve(__dirname, '../src/client/index.jsx'),
+        'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr',
+    ],
     output: {
         path: path.resolve(__dirname, '../dist/client'),
-        filename: 'client.js'
+        filename: 'client.js',
+        publicPath: '/static/',
     },
     module: {
         rules: [{
@@ -28,4 +37,10 @@ module.exports = {
         }]
     },
     devtool: setupDevtools(),
+    plugins: IS_DEV
+        ? [
+        new HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+    ]
+        : [],
 }
